81f7e526ab42f84aeb3105e402d191dfcbcac282
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-05-23 16:44:13 -0500

    fixed segment around create account form

--- src/components/auth/NewUser.js
+++ src/components/auth/NewUser.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Redirect} from 'react-router-dom';
+import { Redirect} from 'react-router-dom';
 import { Button, Form, Grid, Header, Icon, Segment } from 'semantic-ui-react'
 
 export default class NewUser extends Component {
@@ -56,52 +56,50 @@ export default class NewUser extends Component {
       <div className='loader'>
         <Grid container centered columns={3}>
           <Grid.Column>
-            <Segment className='widget'>
+            <Segment>
               <Header as='h2' className='main-widget-header'>
                 <Icon name='add user' size='small'/>
                 <Header.Content>
-                  {'Create Account'}
+                    {'Create Account'}
                 </Header.Content>
               </Header>
-              <Segment>
-                <Form onSubmit={this.handleSubmit}>
-                  <Form.Field>
-                    <label>Username</label>
-                    <input
-                      placeholder='username'
-                      value={this.state.username}
-                      onChange={this.handleChange('username')}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <label>Email Address</label>
-                    <input
-                      placeholder='email'
-                      value={this.state.email}
-                      onChange={this.handleChange('email')}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <label>Password</label>
-                    <input
-                      type='password'
-                      placeholder='password'
-                      value={this.state.password}
-                      onChange={this.handleChange('password')}
-                    />
-                  </Form.Field>
-                  <Form.Field>
-                    <label>Re-Type Password</label>
-                    <input
-                      type='password'
-                      placeholder='re-type password'
-                      value={this.state.passwordReType}
-                      onChange={this.handleChange('passwordReType')}
-                    />
-                  </Form.Field>
-                  <Button type='submit'>Submit</Button>
-                </Form>
-              </Segment>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Field>
+                  <label>Username</label>
+                  <input
+                    placeholder='username'
+                    value={this.state.username}
+                    onChange={this.handleChange('username')}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Email Address</label>
+                  <input
+                    placeholder='email'
+                    value={this.state.email}
+                    onChange={this.handleChange('email')}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <input
+                    type='password'
+                    placeholder='password'
+                    value={this.state.password}
+                    onChange={this.handleChange('password')}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Re-Type Password</label>
+                  <input
+                    type='password'
+                    placeholder='re-type password'
+                    value={this.state.passwordReType}
+                    onChange={this.handleChange('passwordReType')}
+                  />
+                </Form.Field>
+                <Button type='submit'>Submit</Button>
+              </Form>
             </Segment>
           </Grid.Column>
         </Grid>
